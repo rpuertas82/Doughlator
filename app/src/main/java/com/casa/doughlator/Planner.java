9b352ac9379ace41a1f4f417366f51a718ebca1c
rpblanco <rpuertas82@gmail.com>
2015-12-21 17:29:21 +0100

    Se modifica el metodo de generación del nombre del fichero de notas. (Bug corregido al generar recetas con nombres cortos)

--- app/src/main/java/com/casa/doughlator/Planner.java
+++ app/src/main/java/com/casa/doughlator/Planner.java
@@ -39,18 +39,16 @@ public class Planner implements Serializable, Cloneable
     public String composeNotesFileName(String recipeName, String ext)
     {
         Random rnd = new Random();
-        String fileName;
         int rndNumber;
-        int firstBlank;
 
         rndNumber = rnd.nextInt((1000 - 0) + 1) + 0;
 
-        fileName = new String(recipeName).substring(0,4);
+        StringBuilder sb = new StringBuilder(String.valueOf(this.hashCode()));
+        sb.append("_");
+        sb.append(String.format("%04d",rndNumber));
+        sb.append(ext);
 
-        fileName += "_" + String.format("%04d",rndNumber);
-        fileName += ext;
-
-        return fileName;
+        return sb.toString();
     }
 
     public void setNotesFileName(String notesFileName) {
